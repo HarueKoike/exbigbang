93c24e6c73082b4fadb7204bc5634d90b483f4f1
小池春枝 <koikeharue@koikeharue-no-MacBook-Pro.local>
2015-05-05 23:00:23 +0900

    test

--- Bigbang/ViewController.swift
+++ Bigbang/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    var audioPlayer:AVAudioPlayer?
+    @IBOutlet var button:UIButton?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 再生する audio ファイルのパスを取得
+        let audioPath = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("dress", ofType: "m4a")!)
+        
+        // auido を再生するプレイヤーを作成する
+        var audioError:NSError?
+        audioPlayer = AVAudioPlayer(contentsOfURL: audioPath, error:&audioError)
+        
+        // エラーが起きたとき
+        if let error = audioError {
+            println("Error \(error.localizedDescription)")
+        }
+        
+        audioPlayer!.delegate = self
+        audioPlayer!.prepareToPlay()
+        
     }
-
+    
+    // ボタンがタップされた時の処理
+    @IBAction func buttonTapped(sender : AnyObject) {
+        if ( audioPlayer!.playing ){
+            audioPlayer!.stop()
+            button?.setTitle("Stop", forState: UIControlState.Normal)
+        }
+        else{
+            audioPlayer!.play()
+            button?.setTitle("Play", forState: UIControlState.Normal)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
